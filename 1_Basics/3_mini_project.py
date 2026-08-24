4210f0ca407ad2c39889a776afa4bca83c407a15
ayushdutta7318 <ayushdutta7318@gmail.com>
2026-08-24 00:12:05 -0700

    Delete 1_Basics directory

--- 1_Basics/3_mini_project.py
+++
@@ -1,7 +0,0 @@
-# Mini Project:
-
-name = input("Enter your name: ");
-age = int(input("Enter your age: "));
-course = input("Enter course to enroll: ");
-
-print(f"\n-----Student Details------\nname: {name}\nage: {age}\ncourse: {course}");
