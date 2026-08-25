4719d541e3184c43575354e7cb5681bf3623571b
ayushdutta7318 <ayushdutta7318@gmail.com>
2026-08-25 00:52:28 -0700

    Add files via upload

---
+++ 1_Basics/1_intro.py
@@ -0,0 +1,13 @@
+print("Hello world!");
+
+
+"""
+Module: A module in Python is simply a Python file (.py) that contains code—such as variables, functions, classes, or statements—that you can reuse in another Python program.
+"""
+
+import pyjokes;
+
+joke = pyjokes.get_joke();
+print(joke);
+
+# if you write python in terminal: you go into REPL: re-evaluate print loop
---
+++ 1_Basics/2_ps.py
@@ -0,0 +1,16 @@
+# Practice set:
+# 1
+print('''🎶 Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky. ✨
+
+Twinkle, twinkle, little star,
+How I wonder what you are! 🌟
+''');
+
+# 2
+import os;
+
+content = os.listdir();
+print(f"Content of current directory: {content}");
