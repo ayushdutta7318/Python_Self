b42ccd80cce54c74b6ad9f3420a23d9a4b99ed1b
ayushdutta7318 <ayushdutta7318@gmail.com>
2026-08-25 01:52:19 -0700

    Add files via upload

---
+++ Chapter_2/3_type.py
@@ -0,0 +1,18 @@
+# type() fn and typecasting:
+
+a=3;
+print(f"datatype of a is {type(a)}");
+
+b = "string";
+print(type(b));
+
+# typecasting:
+
+a = "5.3";
+b = float(a);
+print(type(a), type(b));
+
+
+# exception in typecasting
+name = "ramesh";
+# print(float(name));valueError
---
+++ Chapter_2/4_input.py
@@ -0,0 +1,12 @@
+# Input fn: Taking input fromt he user:
+
+name = input("enter name: ");
+print(f"Good Morning, {name}");
+
+# datatype stored through input fn is always string:
+
+num_1 = int(input("enter num 1: "));
+num_2 = int(input("enter num 2: "));
+num_3 = int(input("enter num 3: "));
+
+print(f"Sum of 3 numbers: {num_1 + num_2 + num_3}");
