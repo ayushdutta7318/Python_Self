92fe5683d16ea9c3e313474aab7e45a9a87592b5
ayushdutta7318 <ayushdutta7318@gmail.com>
2026-08-26 05:47:31 -0700

    Add files via upload

---
+++ Chapter_2/4_user_input.py
@@ -0,0 +1,9 @@
+#user input
+
+a = input("Enter a: ");
+print(f"a: {a}, datatype: {type(a)}");
+
+# datatype is string, so we need to convert to int
+
+a = int(a);
+print(f"a: {a}, datatype: {type(a)}");
